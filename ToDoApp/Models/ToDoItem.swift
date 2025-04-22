3796cd8b1e3fe11518f7c175c08f2f0c6f06884e
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-04-22 17:01:29 +0700

    Định nghĩa các thông tin cơ bản của một Item

--- ToDoApp/Models/ToDoItem.swift
+++ ToDoApp/Models/ToDoItem.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+// Định nghĩa cấu trúc Subtask
+struct Subtask: Identifiable, Hashable {
+    var id = UUID()
+    var title: String
+    var isComplate: Bool = false
+}
+
+// Định nghĩa cấu trúc ToDoItem
+struct ToDoItem: Identifiable, Hashable {
+    var id = UUID()
+    
+    var title: String                      // Tiêu đề
+    var icon: String                       // Biểu tượng (SF Symbol hoặc tên ảnh)
+    
+    var date: Date                         // Ngày thực hiện
+    var time: Date?                        // Giờ cụ thể (tuỳ chọn)
+    
+    var isRepeating: Bool = false          // Có lặp lại không
+    var repeatFrequency: RepeatFrequency?  // Tần suất lặp lại
+    
+    var reminder: Bool = false             // Có bật nhắc nhở không
+    var tag: String?                       // Gắn thẻ (ví dụ: "Học tập", "Sức khỏe")
+    
+    var subtasks: [Subtask] = []           // Các công việc con
+    var color: Color = .blue               // Màu đại diện cho task
+}
+
+enum RepeatFrequency: String, CaseIterable {
+    case daily
+    case weekly
+    case monthly
+}
+
+
+
