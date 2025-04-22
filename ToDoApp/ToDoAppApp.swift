03c669d85e8cd89d508943d43f8ac4c70f01f7fb
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-04-22 16:23:57 +0700

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Nguyễn Quang Anh on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
